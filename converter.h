e0ee2a9dbf035b30a448239314462ae0d7a54f46
ingralog000 <31670212+ingralog000@users.noreply.github.com>
2017-11-02 15:05:10 -0500

    Update converter.h

--- converter.h
+++ converter.h
@@ -6,3 +6,11 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+float m_to_mi(void)
+{
+    //LOGAN INGRAM
+    float m = get_float("Number of Meters: ");
+    float mi = m / 1,609.344;
+    printf("\nThe Number of Miles is %.2f\n", mi);
+    return in;
+{
